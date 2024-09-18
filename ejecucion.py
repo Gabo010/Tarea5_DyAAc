fe509645e8aaf3bdf38a7be0aa7e652ca92300e7
Gabo <cristhiancarmona32@aragon.unam.mx>
2024-09-17 23:49:37 -0600

    corrección

---
+++ ejecucion.py
@@ -0,0 +1,54 @@
+def busqueda_lineal (data, a_buscar):
+    n= len(data)
+    i = 0
+    while i < len(data):
+        if data[i] == numero:
+            return i
+        i += 1
+    return -1
+
+def busqueda_lineal_centinela (data, a_buscar):
+    data.append(numero)
+
+    i = 0
+    while data[i] != numero:
+        i += 1
+
+    data.pop()
+
+    if i < len(data):
+        return i
+    else:
+        return -1
+
+def busqueda_binaria(data, numero):
+
+    i = 0
+    fin = len(data) - 1
+
+    while i <= fin:
+        medio = (i + fin) // 2
+
+        if data[medio] == numero:
+            return medio
+
+        elif data[medio] > numero:
+            fin = medio - 1
+
+        else:
+            i = medio + 1
+
+    return -1
+
+
+lista = [7,4,37,55,77,104,256,512]
+numero = 104
+resultado = busqueda_binaria(lista, numero)
+
+busqueda_binaria(lista, numero)
+
+
+if resultado != -1:
+    print(f"Elemento encontrado en el índice: {resultado}")
+else:
+    print("Elemento no encontrado")
